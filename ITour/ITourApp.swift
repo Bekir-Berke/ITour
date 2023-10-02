197a5ff80d5db70dda71af6cf67ec44fc116a439
Bekir Berke <76753313+Bekir-Berke@users.noreply.github.com>
2023-10-02 14:56:25 +0300

    Initial Commit

---
+++ ITour/ITourApp.swift
@@ -0,0 +1,17 @@
+//
+//  ITourApp.swift
+//  ITour
+//
+//  Created by Bekir Berke Yılmaz on 2.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ITourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
